194cd4f38ebbda2eef9e65d40f06bb60425691c0
Lucas <lucasge676@gmail.com>
2025-01-20 16:41:09 -0300

    Modulos AsyncFileSystem y http

---
+++ asyncfs.js
@@ -0,0 +1,43 @@
+const fs = require("fs");
+
+/* cuando no ponemos Sync al final de la función estamos ejecutándola de forma asíncrona, por lo que no se la puede almacenar en una variable, aquí específicamente estamos usando la función readFile, donde se compone de parámetros como la ruta de directorio, el tipo de codificación de texto y una función conocida como callback, la cual se ejecutará después de finalizar la ejecución de esta async, también se puede anidar asyncs */
+
+fs.readFile("./data/first.txt", "utf-8", (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log(data);
+
+  fs.readFile("./data/second.txt", "utf-8", (error, data) => {
+    if (error) {
+      console.log(error);
+    }
+    console.log(data);
+    fs.writeFile(
+      "./data/newfile.txt",
+      "archivo creado desde asyncfs",
+      (err, data) => {
+        console.log(error);
+        console.log(data);
+
+        fs.writeFile(
+          "./data/newfile2.txt",
+          "archivo creado desde asyncfs",
+          (err, data) => {
+            console.log(error);
+            console.log(data);
+
+            fs.writeFile(
+              "./data/newfile3.txt",
+              "archivo creado desde asyncfs",
+              (err, data) => {
+                console.log(error);
+                console.log(data);
+              }
+            )
+          }
+        )
+      }
+    )
+  })
+})
---
+++ http.js
@@ -0,0 +1,26 @@
+const http = require("http");
+
+/* Creamos un servidor http, se compone de request y response*/
+const server = http.createServer((request, response) => {
+  console.log(request.url);
+
+  if (request.url === "/") {
+    response.write("Welcome to the Server");
+    return response.end();
+  }
+  if (request.url === "/about") {
+    response.write("Acerca De: ");
+    return response.end();
+  }
+  /* Usando condicionales para cambiar el response según la solicitud (También se le puede pasar html) */
+  response.write(`           
+        <h1>Not Found</h1>
+        <p>No encontramos la pagina que estabas buscando</p>
+        <a href="/">Volver a la pagina principal</a>
+        <img src="https://i.pinimg.com/736x/f0/2c/f2/f02cf231501047982a7cd7f2dea40144.jpg" alt = "Imagen">`); /* Con `` se pueden crear strings de varias lineas */
+  response.end();
+});
+
+server.listen(3000); /*Asigno a mi servidor http el puerto 3000*/
+
+console.log("Servidor escuchando en el puerto 3000");
