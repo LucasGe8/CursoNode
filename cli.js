cd8d7982f56780356578f4e23c443294ad5666e1
Lucas <lucasge676@gmail.com>
2025-01-20 18:57:40 -0300

    modulos con npm

---
+++ cli.js
@@ -0,0 +1,7 @@
+const colors = require('colors')
+
+console.log("Hello World".blue + " utilizando".green + " colors".rainbow)
+
+/* Si por alguna razón se eliminan los módulos de un programa, se pueden recuperar usando npm install sin decirle ningun nombre para que instale automáticamente los módulos que encuentre en las dependencias */
+
+/* npm remove: sirve para eliminar un modulo */
